07bc5bf78299689a28833d1ac73df8b5c8e6660c
David Scobie <david.scobie.20@ucl.ac.uk>
2020-10-09 12:27:40 +0100

    The user can enter the spacings in each dimension and the image stretches accordingly (like on ITK-SNAP).

--- ImageViewer.m
+++ ImageViewer.m
@@ -16,11 +16,35 @@ axial_slice = squeeze(volume(:,:,zcoord));
 %Rotate the slices by 90 degrees (to align with ITK-SNAP)
 coronal_slice = rot90(coronal_slice,1);
 sagittal_slice = rot90(sagittal_slice,1);
-axial_slice = rot90(axial_slice,1);
-%Apply spacings to the slices
-coronal_slice = imresize(coronal_slice,[(181.*spacings(3)) 217]);
+axial_slice = rot90(axial_slice,3);
+
 %finding dimensions of axial slice
 axial_size=size(axial_slice);
+disp(axial_size)
+
+%Apply spacings to the slices
+axial_slice = imresize(axial_slice,[(dimensions(2).*spacings(2)) dimensions(1).*spacings(1)]);
+coronal_slice = imresize(coronal_slice,[(dimensions(3).*spacings(3)) dimensions(2).*spacings(2)]);
+sagittal_slice = imresize(sagittal_slice,[(dimensions(3).*spacings(3)) dimensions(1).*spacings(1)]);
+
+%working out image areas
+%axial bounds
+axial_y_low_bound = round(((dimensions(2).*spacings(2))./2)-(dimensions(2)./2))+1;
+axial_y_up_bound  = round(((dimensions(2).*spacings(2))./2)+(dimensions(2)./2));
+axial_x_low_bound = round(((dimensions(1).*spacings(1))./2)-(dimensions(1)./2))+1;
+axial_x_up_bound  = round(((dimensions(1).*spacings(1))./2)+(dimensions(1)./2));
+
+%coronal bounds
+coronal_z_low_bound = round(((dimensions(3).*spacings(3))./2)-(dimensions(3)./2))+1;
+coronal_z_up_bound  = round(((dimensions(3).*spacings(3))./2)+(dimensions(3)./2));
+coronal_y_low_bound = round(((dimensions(2).*spacings(2))./2)-(dimensions(2)./2))+1;
+coronal_y_up_bound  = round(((dimensions(2).*spacings(2))./2)+(dimensions(2)./2));
+
+%sagittal bounds
+sagittal_z_low_bound = round(((dimensions(3).*spacings(3))./2)-(dimensions(3)./2))+1;
+sagittal_z_up_bound  = round(((dimensions(3).*spacings(3))./2)+(dimensions(3)./2));
+sagittal_x_low_bound = round(((dimensions(1).*spacings(1))./2)-(dimensions(1)./2))+1;
+sagittal_x_up_bound  = round(((dimensions(1).*spacings(1))./2)+(dimensions(1)./2));
 
 % %Image the slices
 % subplot(2,2,1); imagesc(axial_slice)
@@ -28,8 +52,8 @@ axial_size=size(axial_slice);
 % subplot(2,2,3); imagesc(sagittal_slice)
 
 %Image the slices
-subplot(2,2,1); imagesc(axial_slice)
-subplot(2,2,2); imagesc(coronal_slice(181:362,1:217))
-subplot(2,2,3); imagesc(sagittal_slice)
+subplot(2,2,1); imagesc(axial_slice(axial_y_low_bound:axial_y_up_bound,axial_x_low_bound:axial_x_up_bound));
+subplot(2,2,2); imagesc(coronal_slice(coronal_z_low_bound:coronal_z_up_bound,coronal_y_low_bound:coronal_y_up_bound));
+subplot(2,2,3); imagesc(sagittal_slice(sagittal_z_low_bound:sagittal_z_up_bound,sagittal_x_low_bound:sagittal_x_up_bound));
 end
 
