d56ae36deefdffb3ef6600c3c2e4aed0d707bcf2
David Scobie <david.scobie.20@ucl.ac.uk>
2020-11-11 08:34:03 +0000

    adding Q1 files to git

--- Coursework1/Q2_part4/ClearLinesFromAxes.m
+++
@@ -1,6 +0,0 @@
-function ClearLinesFromAxes(axesToClear)
-    axesHandlesToChildObjects = findobj(axesToClear, 'Type', 'line');
-    if ~isempty(axesHandlesToChildObjects)
-      delete(axesHandlesToChildObjects);
-    end  
-    return;
