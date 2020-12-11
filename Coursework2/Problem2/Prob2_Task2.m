63a9fb59fffad63d181e5acee20859e0831c6f09
David Scobie <david.scobie.20@ucl.ac.uk>
2020-12-11 23:48:11 +0000

    combining phase encoding steps for case where they overlap with dephasing of readout

--- Coursework2/Problem2/Prob2_Task2.m
+++ Coursework2/Problem2/Prob2_Task2.m
@@ -3,6 +3,7 @@ clear all
 close all
 set(0,'DefaultLegendAutoUpdate','off')
 
+% for i=5
 for i=1:5
     Gstep=(i-3).*2.3;
 
@@ -20,7 +21,7 @@ for i=1:5
     %The initial spin vector magnitudes
     
     %Uniform square distribution
-    vecMus0=[-11,-6,-1,-11,-6,-1,-11,-6,-1,1,6,11,1,6,11,1,6,11;-11,-11,-11,-6,-6,-6,-1,-1,-1,1,1,1,6,6,6,11,11,11;0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5];
+    vecMus0=[0,-10,-5,-0,-10,-5,-0,-10,-5,-0,0,5,10,0,5,10,0,5,10;0,-10,-10,-10,-5,-5,-5,-0,-0,-0,0,0,0,5,5,5,10,10,10;0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5];
     
     vecMusReset=vecMus0;
     
@@ -87,7 +88,8 @@ for i=1:5
     plotSpin3D_2Dgrid(stage,no_of_spins,t,h1,vecMus0orig,vecMusReset);
 
     %%
-    %Gy is on for Ts/2, this is the phase encoding gradient
+    %Gy and Gx are on for Ts/2, this is the phase encoding gradient and
+    %dephasing lobe of the readout
 
     %Redefine the axes to 2D now
     delete(h1)
@@ -104,7 +106,7 @@ for i=1:5
     hold on;
     xlabel('time (ms)');
     ylabel('M_x,M_y');
-    xlim([0 (2.*Ts)+0.32])
+    xlim([0 (1.5.*Ts)+0.32])
     ylim([-1 1]);
     grid on; 
 
@@ -112,7 +114,7 @@ for i=1:5
     hold on;
     xlabel('time (ms)');
     ylabel('MTrans');
-    xlim([0 (2.*Ts)+0.32])
+    xlim([0 (1.5.*Ts)+0.32])
     ylim([-0.5 0.5]);
     grid on; 
 
@@ -120,7 +122,7 @@ for i=1:5
     hold on;
     xlabel('time (ms)');
     ylabel('G_x,G_y');
-    xlim([0, 0.32+(2.*Ts)]);
+    xlim([0, 0.32+(1.5.*Ts)]);
     ylim([-GMax GMax]);
     grid on; 
 
@@ -128,7 +130,7 @@ for i=1:5
     hold on;
     xlabel('time (ms)');
     ylabel('k_x,k_y');
-    xlim([0, 0.32+(2.*Ts)]);
+    xlim([0, 0.32+(1.5.*Ts)]);
     ylim([-3 3]);
     grid on;
 
@@ -146,15 +148,19 @@ for i=1:5
     T2=10;
 
     stage=2;
-%     Gy1=4.6.*(-256./256);
     Gy1=Gstep;
     kx1=0;
-    Gx1=0;
+    Gx1=-4.6;
 
-    wy=zeros(no_of_spins);
+    wy=zeros(1,no_of_spins);
+    wx=zeros(1,no_of_spins);
+    w=zeros(1,no_of_spins);
     for i = 1:no_of_spins
         wy(i)=Gy1.*(vecMus0orig(2,i));
-    end    
+        wx(i)=Gx1.*(vecMus0orig(1,i));
+        w(i) = wy(i)+wx(i);
+    end
+    
 
 
     for i = 1:N./2
@@ -162,10 +168,8 @@ for i=1:5
         t_from_start=0.32+(i.*(Ts./(N)));
         t=(i.*(Ts./(N)));
         ky1=0.1.*Gy1.*t;
+        kx1=0.1.*Gx1.*t;
 
-        if i == N./2
-            disp(t_from_start)
-        end
 
         for k = 1:no_of_spins
             %Moving initial vector to near origin
@@ -173,8 +177,8 @@ for i=1:5
             vecMus2init(2,k)=vecMus2start(2,k)-vecMus0orig(2,k);
 
             %Bloch equations
-            vecMus2rot(1,k)=exp((-t)./T2).*((vecMus2init(1,k).*cos(wy(k).*t))+((vecMus2init(2,k).*(sin(wy(k).*t)))));
-            vecMus2rot(2,k)=exp((-t)./T2).*((vecMus2init(2,k).*cos(wy(k).*t))-((vecMus2init(1,k).*(sin(wy(k).*t)))));
+            vecMus2rot(1,k)=exp((-t)./T2).*((vecMus2init(1,k).*cos(w(k).*t))+((vecMus2init(2,k).*(sin(w(k).*t)))));
+            vecMus2rot(2,k)=exp((-t)./T2).*((vecMus2init(2,k).*cos(w(k).*t))-((vecMus2init(1,k).*(sin(w(k).*t)))));
 
             %Resetting position to actual
             vecMus2reset(1,k)=vecMus2rot(1,k)+vecMus0orig(1,k);
@@ -221,100 +225,23 @@ for i=1:5
     plotSpin3D_2Dgrid(stage,no_of_spins,t_from_start,h1,vecMus0orig(1:2,:),vecMus2reset);
 
     %%
-    %Gx is on for Ts/2, this is the dephasing lobe of the readout gradient 
-    disp(t_from_start)
+    %-Gx is on for Ts, this is the rephasing lobe of the readout gradient 
+
     vecMus3start=vecMus2reset;
-    Gx2=-4.6;
+    Gx2=4.6;
     ky2=0+ky1;
     Gy2=0;
-    T2=10;
-
+    
     wx=zeros(no_of_spins);
     for i = 1:no_of_spins
         wx(i)=Gx2.*(vecMus0orig(1,i));
     end 
 
-    for i = 1:N./2
-
-        t_from_start=(0.32)+(Ts./2)+(i.*(Ts./(N)));
-        t=(i.*(Ts./(N)));
-        kx2=(0.1.*Gx2.*t)+kx1;
-
-        if i == 1
-            disp(t_from_start)
-        end
-
-        for k = 1:no_of_spins
-            %Moving initial vector to near origin
-            vecMus3init(1,k)=vecMus3start(1,k)-vecMus0orig(1,k);
-            vecMus3init(2,k)=vecMus3start(2,k)-vecMus0orig(2,k);
-
-            %Bloch equations
-            vecMus3rot(1,k)=exp((-t)./T2).*((vecMus3init(1,k).*cos(wx(k).*t))+((vecMus3init(2,k).*(sin(wx(k).*t)))));
-            vecMus3rot(2,k)=exp((-t)./T2).*((vecMus3init(2,k).*cos(wx(k).*t))-((vecMus3init(1,k).*(sin(wx(k).*t)))));
-
-            %Resetting position to actual
-            vecMus3reset(1,k)=vecMus3rot(1,k)+vecMus0orig(1,k);
-            vecMus3reset(2,k)=vecMus3rot(2,k)+vecMus0orig(2,k);       
-        end
-
-        hVecMu = plotSpin3D_2Dgrid(stage,no_of_spins,t_from_start,h1,vecMus0orig(1:2,:),vecMus3reset);
-        legend(h1,'hide')
-
-            %Calculating transverse magnetisation
-        for i = 1:no_of_spins    
-            Mx(i)=vecMus3reset(1,i)-vecMus0orig(1,i);
-            My(i)=vecMus3reset(2,i)-vecMus0orig(2,i);
-        end
-        Mxtot=sum(Mx./(no_of_spins.*mu));
-        Mytot=sum(My./(no_of_spins.*mu));
-
-        hMTrans = plot_Task1_MTrans(t_from_start,h2,Mxtot,Mytot);
-
-        MTrans=Mxtot+Mytot./2;
-        plot(h3,t_from_start,MTrans,'Color', 'k', 'Marker', '.', 'MarkerSize', 10)
-        hold on   
-
-        plot(h4,t_from_start,Gx2,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','M_x');
-        %     legend('k_t_o_t');
-        hold on
-        plot(h4,t_from_start,Gy2,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','M_y');
-        hold on
-
-
-        plot(h5,t_from_start,kx2,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','k_x');
-        %     legend('k_t_o_t');
-        hold on
-        plot(h5,t_from_start,ky2,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','k_y');
-        hold on    
-
-        plot_kx_ky(h6,kx2,ky2)
-
-        pause(0.01);
-
-        ClearLinesFromAxes(h1);
-    end
-    plotSpin3D_2Dgrid(stage,no_of_spins,t_from_start,h1,vecMus0orig(1:2,:),vecMus3reset);
-
-    %%
-    %-Gx is on for Ts, this is the rephasing lobe of the readout gradient 
-
-    vecMus3start=vecMus3reset;
-    Gx3=4.6;
-    ky3=0+ky2;
-    Gy3=0;
-    T2=10;
-
-    wx=zeros(no_of_spins);
-    for i = 1:no_of_spins
-        wx(i)=Gx3.*(vecMus0orig(1,i));
-    end 
-
     for i = 1:N
 
-        t_from_start=(0.32)+(Ts./2)+(Ts./2)+(i.*(Ts./N));
+        t_from_start=(0.32)+(Ts./2)+(i.*(Ts./N));
         t=(i.*(Ts./N));
-        kx3=(0.1.*Gx3.*t)+kx2;
+        kx2=(0.1.*Gx2.*t)+kx1;
 
         for k = 1:no_of_spins
             %Moving initial vector to near origin
@@ -347,20 +274,20 @@ for i=1:5
         plot(h3,t_from_start,MTrans,'Color', 'k', 'Marker', '.', 'MarkerSize', 10)
         hold on  
 
-        plot(h4,t_from_start,Gx3,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','M_x');
+        plot(h4,t_from_start,Gx2,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','M_x');
         %     legend('k_t_o_t');
         hold on
-        plot(h4,t_from_start,Gy3,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','M_y');
+        plot(h4,t_from_start,Gy2,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','M_y');
         hold on
 
 
-        plot(h5,t_from_start,kx3,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','k_x');
+        plot(h5,t_from_start,kx2,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','k_x');
         %     legend('k_t_o_t');
         hold on
-        plot(h5,t_from_start,ky3,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','k_y');
+        plot(h5,t_from_start,ky2,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','k_y');
         hold on    
 
-        plot_kx_ky(h6,kx3,ky3)
+        plot_kx_ky(h6,kx2,ky2)
 
         pause(0.01);
 
---
+++ Coursework2/Problem2/plot_Task1_MTrans.m
@@ -0,0 +1,27 @@
+function objHandle = plot_Task1_MTrans(t,figHandle,Mx,My,colors)
+%PLOTSPIN3D This function plots the 3D precession of a spin precessing
+%around the z axis in a magnetic field.
+%   To be used with worksheet1 script.
+%assigning colors to the plots.
+%if nargin <3
+colors=['b','r'];
+%end
+%Plotting the vectors of the spins.
+
+
+objHandle(1)=plot(figHandle, t, Mx,...
+    'Color',colors(1), 'Marker', '.', 'MarkerSize', 10, 'DisplayName','M_x');
+hold on
+%Plotting the points at the end of the spin vectors.
+objHandle(2)=plot(figHandle,t,My,...
+    'Color', colors(2), 'Marker', '.', 'MarkerSize', 10, 'DisplayName','M_y');
+hold on
+
+
+% legend;
+
+
+% title(['t=',num2str(t,'%.2f'),'ms'])
+
+end
+
