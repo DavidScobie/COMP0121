9c2fe353325b3fd9b9df435b69708678f6a0991d
David Scobie <david.scobie.20@ucl.ac.uk>
2020-12-11 19:45:35 +0000

    getting all of the plots for 1 phase encoding step

--- Coursework2/Problem2/Prob2_Task1.m
+++ Coursework2/Problem2/Prob2_Task1.m
@@ -7,7 +7,7 @@ mu=0.5;
 theta=0;
 phi=pi./2;
 
-N=300;
+N=100;
 GMax=4.6;
 Ts=5.12;
 noOfSteps = 16;
@@ -116,18 +116,43 @@ xlim([0 (2.*Ts)+0.32])
 ylim([-0.5 0.5]);
 grid on; 
 
+h4 = subplot(2,3,4);
+hold on;
+xlabel('time (ms)');
+ylabel('G_x,G_y');
+xlim([0, 0.32+(2.*Ts)]);
+ylim([-GMax GMax]);
+grid on; 
+
+h5 = subplot(2,3,5);
+hold on;
+xlabel('time (ms)');
+ylabel('k_x,k_y');
+xlim([0, 0.32+(2.*Ts)]);
+ylim([-3 3]);
+grid on;
+
+h6 = subplot(2,3,6);
+hold on;
+xlabel('k_x');
+ylabel('k_y');
+xlim([-2 2]);
+ylim([-2 2]);
+grid on;
+
 set(0,'DefaultLegendAutoUpdate','off')
 
 vecMus2start=vecMusReset(1:2,:);
 T2=10000;
 
 stage=2;
-Gy=4.6.*(-256./256);
-% Gy=0;
+Gy1=4.6.*(-256./256);
+kx1=0;
+Gx1=0;
 
 wy=zeros(no_of_spins);
 for i = 1:no_of_spins
-    wy(i)=Gy.*(vecMus0orig(2,i));
+    wy(i)=Gy1.*(vecMus0orig(2,i));
 end    
 
 
@@ -135,7 +160,11 @@ for i = 1:N./2
     
     t_from_start=0.32+(i.*(Ts./(N)));
     t=(i.*(Ts./(N)));
-    ky1=0.1.*Gy.*t;
+    ky1=0.1.*Gy1.*t;
+    
+    if i == N./2
+        disp(t_from_start)
+    end
     
     for k = 1:no_of_spins
         %Moving initial vector to near origin
@@ -168,6 +197,22 @@ for i = 1:N./2
     plot(h3,t_from_start,MTrans,'Color', 'k', 'Marker', '.', 'MarkerSize', 10)
     hold on
     
+    
+    plot(h4,t_from_start,Gx1,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','M_x');
+    %     legend('k_t_o_t');
+    hold on
+    plot(h4,t_from_start,Gy1,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','M_y');
+    hold on
+    
+    
+    plot(h5,t_from_start,kx1,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','k_x');
+    %     legend('k_t_o_t');
+    hold on
+    plot(h5,t_from_start,ky1,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','k_y');
+    hold on
+    
+    plot_kx_ky(h6,kx1,ky1)
+    
     pause(0.001);
     
     ClearLinesFromAxes(h1);
@@ -176,21 +221,27 @@ plotSpin3D_2Dgrid(stage,no_of_spins,t_from_start,h1,vecMus0orig(1:2,:),vecMus2re
 
 %%
 %Gx is on for Ts/2, this is the dephasing lobe of the readout gradient 
-
+disp(t_from_start)
 vecMus3start=vecMus2reset;
-Gx=-4.6;
+Gx2=-4.6;
+ky2=0+ky1;
+Gy2=0;
 T2=10;
 
 wx=zeros(no_of_spins);
 for i = 1:no_of_spins
-    wx(i)=Gx.*(vecMus0orig(1,i));
+    wx(i)=Gx2.*(vecMus0orig(1,i));
 end 
 
 for i = 1:N./2
     
     t_from_start=(0.32)+(Ts./2)+(i.*(Ts./(N)));
     t=(i.*(Ts./(N)));
-%     kx1=0.1.*Gx.*t;
+    kx2=(0.1.*Gx2.*t)+kx1;
+    
+    if i == 1
+        disp(t_from_start)
+    end
     
     for k = 1:no_of_spins
         %Moving initial vector to near origin
@@ -221,7 +272,22 @@ for i = 1:N./2
     
     MTrans=Mxtot+Mytot./2;
     plot(h3,t_from_start,MTrans,'Color', 'k', 'Marker', '.', 'MarkerSize', 10)
+    hold on   
+    
+    plot(h4,t_from_start,Gx2,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','M_x');
+    %     legend('k_t_o_t');
+    hold on
+    plot(h4,t_from_start,Gy2,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','M_y');
+    hold on
+    
+    
+    plot(h5,t_from_start,kx2,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','k_x');
+    %     legend('k_t_o_t');
+    hold on
+    plot(h5,t_from_start,ky2,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','k_y');
     hold on    
+    
+    plot_kx_ky(h6,kx2,ky2)
     
     pause(0.01);
     
@@ -233,19 +299,21 @@ plotSpin3D_2Dgrid(stage,no_of_spins,t_from_start,h1,vecMus0orig(1:2,:),vecMus3re
 %-Gx is on for Ts, this is the rephasing lobe of the readout gradient 
 
 vecMus3start=vecMus3reset;
-Gx=4.6;
+Gx3=4.6;
+ky3=0+ky2;
+Gy3=0;
 T2=10;
 
 wx=zeros(no_of_spins);
 for i = 1:no_of_spins
-    wx(i)=Gx.*(vecMus0orig(1,i));
+    wx(i)=Gx3.*(vecMus0orig(1,i));
 end 
 
 for i = 1:N
     
     t_from_start=(0.32)+(Ts./2)+(Ts./2)+(i.*(Ts./N));
     t=(i.*(Ts./N));
-    kx1=0.1.*Gx.*t;
+    kx3=(0.1.*Gx3.*t)+kx2;
     
     for k = 1:no_of_spins
         %Moving initial vector to near origin
@@ -276,7 +344,22 @@ for i = 1:N
     
     MTrans=Mxtot+Mytot./2;
     plot(h3,t_from_start,MTrans,'Color', 'k', 'Marker', '.', 'MarkerSize', 10)
+    hold on  
+    
+    plot(h4,t_from_start,Gx3,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','M_x');
+    %     legend('k_t_o_t');
+    hold on
+    plot(h4,t_from_start,Gy3,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','M_y');
+    hold on
+    
+    
+    plot(h5,t_from_start,kx3,'Color','b','Marker', '.', 'MarkerSize', 10,'DisplayName','k_x');
+    %     legend('k_t_o_t');
+    hold on
+    plot(h5,t_from_start,ky3,'Color','r','Marker', '.', 'MarkerSize', 10,'DisplayName','k_y');
     hold on    
+    
+    plot_kx_ky(h6,kx3,ky3)
     
     pause(0.01);
     
